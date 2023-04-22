955e56c35d82abee09ddc158f30d32ebac6854b9
marti <martines.607@gmail.com>
2023-04-22 18:39:49 -0500

    initan commit

--- src/main/java/com/Safra/defectrecord/controllers/FabricReportController.java
+++ src/main/java/com/Safra/defectrecord/controllers/FabricReportController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/fabricReport")
 public class FabricReportController {
 
